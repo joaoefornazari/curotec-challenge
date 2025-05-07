9bec863e17309a32447626af8aa08213962dfed1
joaoefornazari <joaoefornazari@gmail.com>
2025-05-07 15:56:39 -0300

    feat: sidemenu container start

--- resources/js/components/user/UserSidemenu.vue
+++ resources/js/components/user/UserSidemenu.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-
+    <div class="sidemenu">
+        <div class="sidemenu__header">
+            <div class="user-icon">
+            </div>
+            <div class="username">
+                <p>Username</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
 
 </script>
+
+<style scoped lang="css">
+@import '@css/components/user/sidemenu.css';
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,9 +1,15 @@
 <template>
-    <UserSidemenu />
-    <MainPanel />
+    <div class="home">
+        <UserSidemenu />
+        <MainPanel />
+    </div>
 </template>
 
 <script setup>
 import UserSidemenu from '@/components/user/UserSidemenu.vue'
 import MainPanel from '@/components/dashboard/MainPanel.vue'
 </script>
+
+<style scoped lang="css">
+@import '@css/pages/home.css';
+</style>
