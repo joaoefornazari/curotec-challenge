6a7b62005dfb466f2fe3715445a24ecd6c4f3ba6
joaoefornazari <joaoefornazari@gmail.com>
2025-05-08 13:17:48 -0300

    feat: fetching projects from backend

--- resources/js/components/project/ProjectArea.vue
+++ resources/js/components/project/ProjectArea.vue
@@ -13,6 +13,20 @@
 <script setup>
 import ProjectHeader from '@/components/project/ProjectHeader.vue'
 import ProjectCard from '@/components/project/ProjectCard.vue'
+
+import { ref, onMounted } from 'vue'
+import Http from '@/tools/api.js'
+
+const projects = ref([])
+
+onMounted(async () => {
+    try {
+        const data = await Http.GET('/projects')
+        projects.value = data
+    } catch (error) {
+        console.error(error)
+    }
+})
 </script>
 
 <style scoped lang="css">
