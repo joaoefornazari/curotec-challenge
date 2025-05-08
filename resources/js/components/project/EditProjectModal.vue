33c60b39405c7f453ba6fe60d649e510c28c82b0
joaoefornazari <joaoefornazari@gmail.com>
2025-05-08 20:34:54 -0300

    feat: modal to edit project details

---
+++ resources/js/components/project/EditProjectModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="modal-overlay">
+    <div class="modal">
+      <h2>Edit Project</h2>
+      <form @submit.prevent="handleSave">
+        <div class="form-group">
+          <label for="project-name">Name</label>
+          <input id="project-name" v-model="formData.name" type="text" required />
+        </div>
+        <!-- <div class="form-group">
+          <label for="project-category">Category</label>
+          <input id="project-category" v-model="formData.category" type="text" required />
+        </div> -->
+        <div class="modal-actions">
+          <button type="submit" class="btn-primary" @click.prevent="handleSave()">SAVE</button>
+          <button type="button" class="btn-secondary" @click.prevent="handleClose()">CANCEL</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, watch } from 'vue'
+
+const { project } = defineProps({
+  project: {
+    required: true,
+  },
+})
+
+const formData = reactive({
+  name: project.name,
+  category: project.category,
+})
+
+const $emits = defineEmits(['save', 'close'])
+
+watch(
+  () => project,
+  (newProject) => {
+    formData.name = newProject.name;
+    formData.category = newProject.category;
+  }
+)
+
+function handleClose() {
+    formData.name = ''
+    formData.category = ''
+
+    $emits('close')
+}
+
+function handleSave() {
+  $emits('save', formData)
+}
+</script>
+
+<style scoped>
+@import '@css/components/project/edit-project-modal.css';
+</style>
--- resources/js/components/project/ProjectCard.vue
+++ resources/js/components/project/ProjectCard.vue
@@ -8,7 +8,7 @@
             <div class="project-card-header">
                 <h3>{{ project.name }}</h3>
                 <div class="project-card-buttons">
-                    <button class="btn-primary project-card-button" @click.stop="toggleExpand">Edit</button>
+                    <button class="btn-primary project-card-button" @click.stop="openEditModal">Edit</button>
                     <button class="btn-danger project-card-button" @click.stop="toggleExpand">Delete</button>
                 </div>
             </div>
@@ -17,25 +17,44 @@
         </div>
         <TaskList v-if="isExpanded" :project-id="project.id"/>
     </div>
+    <EditProjectModal v-if="isEditModalVisible" :project="project" @close="closeEditModal" @save="onSave" />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import TaskList from '@/components/task/TaskList.vue'
+import EditProjectModal from '@/components/project/EditProjectModal.vue'
+import { useProjectForm } from '@/scripts/composables/project/useProjectForm.js';
 
-const { project } = defineProps({
+const props = defineProps({
     project: {
         required: true,
     }
 })
 
-const isExpanded = ref(false);
+const { project, save } = useProjectForm(props.project);
+
+const isExpanded = ref(false)
+const isEditModalVisible = ref(false)
 
 function toggleExpand() {
-    isExpanded.value = !isExpanded.value;
+    isExpanded.value = !isExpanded.value
+}
+
+function openEditModal() {
+    isEditModalVisible.value = true
+}
+
+function closeEditModal() {
+    isEditModalVisible.value = false
+}
+
+async function onSave(updatedProject) {
+    await save(updatedProject, props.project.id)
+    closeEditModal()
 }
 </script>
 
 <style lang="css" scoped>
-@import '@css/components/project/project-card.css';
+@import '@css/components/project/project-card.css'
 </style>
